10666dba39db9090a302325168d5210db25888f4
Marwam Malta <marwamprog.js@gmail.com>
2024-01-12 21:30:11 -0300

    feat: Migrate finalizado. Iniciando com Seeder

--- app_super_gestao/app/Fornecedor.php
+++ app_super_gestao/app/Fornecedor.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fornecedor extends Model
 {
-    //
+    //Serve para não deletar dados da tabela. Apenas faz o controle por campo
+    use SoftDeletes;
+
+    protected $table = 'fornecedores';
+
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
