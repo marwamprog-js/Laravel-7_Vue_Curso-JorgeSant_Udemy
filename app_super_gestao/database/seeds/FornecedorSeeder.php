a2c4566c31294a2ddee824ca28a30f055fb020f1
Marwam Malta <marwamprog.js@gmail.com>
2024-01-12 21:39:23 -0300

    feat: Cadastrando com Seeder (php artisan db:seed)

--- app_super_gestao/database/seeds/FornecedorSeeder.php
+++ app_super_gestao/database/seeds/FornecedorSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Fornecedor;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class FornecedorSeeder extends Seeder
 {
@@ -11,6 +13,29 @@ class FornecedorSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //Instanciando
+        $fornecedor = new Fornecedor();
+        $fornecedor->nome = 'Fornecedor 100';
+        $fornecedor->site = 'fornecedor100.com.br';
+        $fornecedor->uf = 'CE';
+        $fornecedor->email = 'contato@fornecedor100.com.br';
+        $fornecedor->save();
+
+        //Estatico
+        Fornecedor::create([
+            'nome' => 'Fornecedor 200',
+            'site' => 'fornecedor200.com.br',
+            'uf' => 'MG',
+            'email' => 'contato@fornecedor200.com.br'
+        ]);
+
+        //Insert atraves da classe DB
+        DB::table('fornecedores')->insert([
+            'nome' => 'Fornecedor 300',
+            'site' => 'fornecedor300.com.br',
+            'uf' => 'SP',
+            'email' => 'contato@fornecedor300.com.br'
+        ]);
+
     }
 }
