01952de3711889f0acb3657c98ba9635133d1d11
Marwam Malta <marwamprog.js@gmail.com>
2024-01-16 11:43:30 -0300

    feat: transformando formulario de produtos em component blade

--- app_super_gestao/resources/views/app/produto/edit.blade.php
+++ app_super_gestao/resources/views/app/produto/edit.blade.php
@@ -20,28 +20,8 @@
     <div class="informacao-pagina"> 
         {{ $msg ?? '' }}
         <div style="width: 30%; margin-left: auto; margin-right: auto;">
-            <form action="{{ route('produto.update', ['produto' => $produto->id]) }}" method="post">
-                @csrf
-                @method('PUT')
-                <input type="text" name="nome" placeholder="Nome" value="{{ $produto->nome ?? old('nome') }}" class="borda-preta">
-                {{ $errors->has('nome') ? $errors->first('nome') : '' }}
-
-                <input type="text" name="descricao" placeholder="Descrição" value="{{ $produto->descricao ??  old('descricao') }}" class="borda-preta">
-                {{ $errors->has('descricao') ? $errors->first('descricao') : '' }}
-
-                <input type="text" name="peso" placeholder="Peso" value="{{ $produto->peso ??  old('peso') }}" class="borda-preta">
-                {{ $errors->has('peso') ? $errors->first('peso') : '' }}
-
-                <select name="unidade_id" id="unidade_id">
-                    <option>-- Selecione a unidade de medida --</option>
-                    @foreach ($unidades as $unidade)
-                        <option value="{{ $unidade->id }}" {{ $produto->unidade_id ?? old('unidade_id') == $unidade->id ? 'selected' : '' }} >{{ $unidade->descricao }}</option>                        
-                    @endforeach
-                </select>
-                {{ $errors->has('unidade_id') ? $errors->first('unidade_id') : '' }}
-
-                <button type="submit" class="borda-preta">Atualizar</button>
-            </form>
+            @component('app.produto._components.form_create_edit', ['produto' => $produto, 'unidades' => $unidades])
+            @endcomponent
         </div>
     </div>
 </div>
